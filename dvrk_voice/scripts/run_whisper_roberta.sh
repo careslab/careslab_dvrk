1a604b803e669d7a60bce97ffa634c978d0d3e32
Blaine <blaine.oania@gmail.com>
2025-07-25 12:38:09 -0400

    modification shell whisper_roberta

--- dvrk_voice/scripts/run_whisper_roberta.sh
+++ dvrk_voice/scripts/run_whisper_roberta.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "source transformers-env/bin/activate && python whisper_roberta.py -l && python whisper_roberta.py -d 10; exec bash"
+gnome-terminal -- bash -c "source ./data/transf_venv/bin/activate && python whisper_roberta.py -l && python whisper_roberta.py -d 10; exec bash"
