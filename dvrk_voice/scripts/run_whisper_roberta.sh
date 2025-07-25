b67112319149852183562eddf3cd4a219e7c82bd
Blaine <blaine.oania@gmail.com>
2025-07-25 12:44:35 -0400

    shell updated

--- dvrk_voice/scripts/run_whisper_roberta.sh
+++ dvrk_voice/scripts/run_whisper_roberta.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "source ./data/transf_venv/bin/activate && python whisper_roberta.py -l && python whisper_roberta.py -d 10; exec bash"
+
+gnome-terminal -- bash -c "cd /home/careslab/careslab_dvrk/dvrk_voice/scripts && source ./data/transf_venv/bin/activate && python whisper_roberta.py -l && python whisper_roberta.py -d 10; exec bash"
