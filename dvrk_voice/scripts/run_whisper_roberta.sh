b963aea9dae37a2c33db9a6a595694ff2649a168
cares.lab@gmail.com <cares.lab@gmail.com>
2025-07-29 17:42:04 -0400

    modif shell again

--- dvrk_voice/scripts/run_whisper_roberta.sh
+++ dvrk_voice/scripts/run_whisper_roberta.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "cd ~/catkin_ws/src/careslab_dvrk/dvrk_voice/scripts && python whisper_roberta.py -l && python whisper_roberta.py -d 10; exec bash"
+source ./data/transformers-env/bin/activate
+python whisper_roberta.py -l
+python whisper_roberta.py -d 10
