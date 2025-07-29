80ee4ab54a2678709e2c65c7138a68d0b98ba76c
cares.lab@gmail.com <cares.lab@gmail.com>
2025-07-29 17:37:46 -0400

    modif run whisper roberta

--- dvrk_voice/scripts/run_whisper_roberta.sh
+++ dvrk_voice/scripts/run_whisper_roberta.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "cd /home/careslab/careslab_dvrk/dvrk_voice/scripts && python whisper_roberta.py -l && python whisper_roberta.py -d 10; exec bash"
+gnome-terminal -- bash -c "cd ~/catkin_ws/src/careslab_dvrk/dvrk_voice/scripts && python whisper_roberta.py -l && python whisper_roberta.py -d 10; exec bash"
